3b3a3a8c2ed521baeefeb76fb8cb848440de6f9d
agendaelectronica <manuelalcantara01@gmail.com>
2023-03-27 19:40:46 -0400

    probandopat

---
+++ src/main/java/com/mycompany/agenteelectronica/AgenteElectronica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.agenteelectronica;
+
+/**
+ *
+ * @author Manu
+ */
+public class AgenteElectronica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
